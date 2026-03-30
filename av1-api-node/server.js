fbb575843b1661072a5cc3bc5e9ba5424f8214d5
Pedro Oliveira Rios Ramalho <pedro.oramalho@eaportal.org>
2026-03-30 09:54:50 -0300

    30/03

--- av1-api-node/server.js
+++ av1-api-node/server.js
@@ -55,3 +55,18 @@ app.delete("/tarefas/:id", (req, res) => {
 app.listen(PORTA, () => {
   console.log(`Servidor rodando na porta ${PORTA}`);
 });
+app.patch("/tarefas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const tarefa = tarefas.find(t => t.id === id);
+
+  if (!tarefa) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+
+  const { titulo, concluida } = req.body;
+
+  if (titulo !== undefined) tarefa.titulo = titulo;
+  if (concluida !== undefined) tarefa.concluida = concluida;
+
+  res.json(tarefa);
+});
